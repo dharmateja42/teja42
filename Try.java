7f8fe35ad1fd2ae5bdf2862049c3d7d19154ec7a
unknown <dharmateja42>
2022-10-11 12:09:59 +0530

    Try.java

---
+++ Try.java
@@ -0,0 +1,16 @@
+class Try
+{
+public static void main(String [] args)
+{
+	try
+	{
+		int x=10,y=5,z;
+		z=x/y;
+		System.out.println(z);
+	}
+	catch(Exception a)
+	{
+		System.out.println(a);
+	}
+}
+}
