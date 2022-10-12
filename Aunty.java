77265b5c5959afe078e3840c25692b76176d9cca
teja <dharmateja42>
2022-10-12 12:34:13 +0530

    commit

--- Aunty.java
+++ Aunty.java
@@ -1,7 +1,7 @@
 public class Aunty {
     public static void main(String[] args)
     {
-        System.out.ptintln("Hello Auntyyyy....!!!");
+        System.out.ptintln("Hello Auntyyyy....!!!");//java
 
     } 
     
